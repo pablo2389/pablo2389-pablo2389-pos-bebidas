bd94d76f0e022b013d0c6bb4b2c1decaf2c2dbf3
pablo2389 <160980965+pablo2389@users.noreply.github.com>
2026-05-03 20:38:18 -0300

    pruebas en main.py

--- bebidas-api/main.py
+++ bebidas-api/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
     allow_origins=[
         "https://kiosco-grace.vercel.app",
     ],
-    allow_credentials=True,
+allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
