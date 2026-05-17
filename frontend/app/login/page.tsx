f3690e2d71a0de232a7f968f6abf37e2f448f3db
pablo2389 <160980965+pablo2389@users.noreply.github.com>
2026-05-16 21:28:34 -0300

    arreglo login

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -63,7 +63,7 @@ export default function Page() {
       localStorage.setItem("token", res.token);
       localStorage.setItem("usuario_nombre", res.nombre || "");
 
-      router.replace("/");
+    router.replace("/pos");
     } catch (err: any) {
       console.error("❌ Error en login:", err);
       setError(err.message || "Error inesperado");
